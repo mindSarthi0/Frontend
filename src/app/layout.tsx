ba41188b0e5243031c05d204cbffdd5110d2dd52
Ashwani Arya <ashwaniparker@gmail.com>
2024-11-23 17:00:36 +0530

    feat : added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-GGBJ26KTBW" />
     </html>
   );
 }
